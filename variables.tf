0b9515e7252b69c7b6d6d4b9817a07e792adbbdd
mark.gyorgy@crayon.com <mark.gyorgy@crayon.com>
2025-03-02 18:14:41 +0100

    add key vault related terraform files

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-gyorgy-vault-dev"
+}
+
+variable "location" {
+  default = "Switzerland North"
+}
+
+variable "subscription_id" {
+  default = "1c2b3b73-e5d4-4ab6-9ac4-c807cb39e0ef"
+}
---
+++ vault.tf
@@ -0,0 +1,9 @@
+resource "azurerm_key_vault" "vault" {
+  name = "vault1216516"
+  location = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  enable_rbac_authorization = true
+  sku_name = "Standard"
+}
